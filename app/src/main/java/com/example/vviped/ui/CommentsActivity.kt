4fabd029193fb6421a7446b6833de0cb51745423
mauraqp <56211605+mauraqp@users.noreply.github.com>
2020-09-24 11:39:14 +0700

    mora's update

--- app/src/main/java/com/example/vviped/ui/CommentsActivity.kt
+++ app/src/main/java/com/example/vviped/ui/CommentsActivity.kt
@@ -1,12 +1,32 @@
 package com.example.vviped.ui
 
+<<<<<<< HEAD
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vviped.LoginActivity
+import com.example.vviped.R
+import com.example.vviped.comment_action
+import kotlinx.android.synthetic.main.activity_comments.*
+import kotlinx.android.synthetic.main.activity_landing.*
+=======
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vviped.R
+>>>>>>> 2482bde56db7d78386b5c5362cdfd812072d25c2
 
 class CommentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comments)
+<<<<<<< HEAD
+
+        comment_act.setOnClickListener{
+            startActivity(Intent(this, comment_action::class.java))
+        }
+
+
+=======
+>>>>>>> 2482bde56db7d78386b5c5362cdfd812072d25c2
     }
 }
