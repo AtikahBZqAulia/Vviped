72215b3673bb513c145a02f6164d13ab7a6db035
AtikahBZqAulia <atikahap.yes@gmail.com>
2020-09-28 20:25:55 +0700

    Update CommentsActivity.kt

--- app/src/main/java/com/example/vviped/ui/CommentsActivity.kt
+++ app/src/main/java/com/example/vviped/ui/CommentsActivity.kt
@@ -18,4 +18,19 @@ class CommentsActivity : AppCompatActivity() {
             startActivity(Intent(this, comment_action::class.java))
         }
     }
+ fun getCommentAPI(){
+        val retro = Retro().getRetroClientInstance().create(CommentAPI::class.java)
+        retro.getComment().enqueue(object : retrofit2.Callback<List<Comment>> {
+            override fun onResponse(call: Call<List<Comment>>, response: Response<List<Comment>>) {
+                val comment = response.body()
+                for (c in comment!!){
+                    Log.e("Result : ", c.email.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<List<Comment>>, t: Throwable) {
+                Log.e("error", t.message.toString())
+            }
+
+        })
 }
