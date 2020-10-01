9c821fece119d776720ce42033e13c28e5c10dcd
AtikahBZqAulia <atikahap.yes@gmail.com>
2020-10-01 14:45:01 +0700

    Update ProfileFragment.kt

--- app/src/main/java/com/example/vviped/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/vviped/ui/profile/ProfileFragment.kt
@@ -13,6 +13,7 @@ import com.example.vviped.R
 import com.example.vviped.model.GridPhotosAdapter
 import com.example.vviped.model.PostAdapter
 import com.example.vviped.ui.CommentsActivity
+import com.example.vviped.Setting
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 
