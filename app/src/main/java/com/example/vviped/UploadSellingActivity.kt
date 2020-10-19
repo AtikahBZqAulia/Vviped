44d53eea169e510e7c054ba4524d8c7430945a8c
Maura Qoonitah <56211605+mauraqoonitah@users.noreply.github.com>
2020-10-19 15:42:25 +0700

    Update UploadSellingActivity.kt

--- app/src/main/java/com/example/vviped/UploadSellingActivity.kt
+++ app/src/main/java/com/example/vviped/UploadSellingActivity.kt
@@ -19,97 +19,11 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 
-class UploadSellingActivity : AppCompatActivity(), UploadRequestBody.UploadCallback {
+class UploadSellingActivity : AppCompatActivity() {
 
-    private var selectedImageUri: Uri? = null
-
-    @RequiresApi(Build.VERSION_CODES.KITKAT)
+  
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload_selling)
-
-        image_view.setOnClickListener {
-            openImageChooser()
-        }
-
-        button_upload.setOnClickListener {
-            uploadImage()
-        }
     }
-
-
-
-    private fun openImageChooser() {
-        Intent(Intent.ACTION_PICK).also {
-            it.type = "image/*"
-            val mimeTypes = arrayOf("image/jpeg", "image/png")
-            it.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
-            startActivityForResult(it, REQUEST_CODE_PICK_IMAGE)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            when (requestCode) {
-                REQUEST_CODE_PICK_IMAGE -> {
-                    selectedImageUri = data?.data
-                    image_view.setImageURI(selectedImageUri)
-                }
-            }
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.KITKAT)
-    private fun uploadImage() {
-        if (selectedImageUri == null) {
-            layout_root.snackbar("Select an Image First")
-            return
-        }
-
-
-        val parcelFileDescriptor = contentResolver.openFileDescriptor(selectedImageUri!!, "r", null) ?: return
-
-        val inputStream = FileInputStream(parcelFileDescriptor.fileDescriptor)
-        val file = File(cacheDir, contentResolver.getFileName(selectedImageUri!!))
-        val outputStream = FileOutputStream(file)
-        inputStream.copyTo(outputStream)
-
-        progress_bar.progress = 0
-        val body = UploadRequestBody(file, "image", this)
-        RetrofitInterface().uploadImage(
-            MultipartBody.Part.createFormData(
-                "image",
-                file.name,
-                body
-            ),
-            RequestBody.create(MediaType.parse("multipart/form-data"), "json")
-        ).enqueue(object : Callback<UploadResponse> {
-            override fun onFailure(call: Call<UploadResponse>, t: Throwable) {
-                layout_root.snackbar(t.message!!)
-                progress_bar.progress = 0
-            }
-
-            override fun onResponse(
-                call: Call<UploadResponse>,
-                response: Response<UploadResponse>
-            ) {
-                response.body()?.let {
-                    layout_root.snackbar(it.message)
-                    progress_bar.progress = 100
-                }
-            }
-        })
-
-    }
-
-    override fun onProgressUpdate(percentage: Int) {
-        progress_bar.progress = percentage
-    }
-
-    companion object {
-        const val REQUEST_CODE_PICK_IMAGE = 101
-    }
-
-
 }
