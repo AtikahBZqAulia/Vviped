a40d064e22f75852cf8cc0ea6b7ffbe00b7cced3
mauraqp <mauraqoonitah@gmail.com>
2020-10-20 01:35:52 +0700

    dynamic chat message-seller buyer

--- app/src/main/java/com/example/vviped/MainChat.kt
+++ app/src/main/java/com/example/vviped/MainChat.kt
@@ -1,56 +1,77 @@
 package com.example.vviped
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.media.MediaScannerConnection
-import android.os.Build
+
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_chat.*
 import com.example.vviped.model.Chat
-import com.example.vviped.AdapterChat
-import kotlinx.android.synthetic.main.activity_register.*
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainChat : AppCompatActivity() {
+    private lateinit var listViewType: MutableList<Int>
+    private lateinit var listChat: MutableList<Chat>
+    private lateinit var adapterChat: AdapterChat
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        BackFromChat.setOnClickListener{
+        btn_BackFromChat.setOnClickListener {
             onBackPressed()
         }
 
-        val listViewType = mutableListOf<Int>()
-        listViewType.add(1)
-        listViewType.add(2)
-        listViewType.add(2)
-        listViewType.add(1)
-        listViewType.add(2)
-        listViewType.add(2)
-        listViewType.add(1)
-        listViewType.add(2)
-        val listChat = mutableListOf<Chat>()
-        listChat.add(Chat(message = "Halo kak, bisa kurang lagi gak?", dateTime = "16:36 PM "))
-        listChat.add(Chat(message = "Yaah, gabisa sis maaf.. ", dateTime = "16:40 PM "))
-        listChat.add(Chat(message = "Harga sudah nett", dateTime = "16:40 PM "))
-        listChat.add(Chat(message = "ooh, yaudah deh kak kalau gitu saya ambil aja", dateTime = "16:41 PM "))
-        listChat.add(Chat(message = "Tolong diisi ya sis alamat untuk pengiriman", dateTime = "16:42 PM "))
-        listChat.add(Chat(message = "Nama, Alamat, No.Telp: ", dateTime = "16:42 PM "))
-        listChat.add(Chat(message = "Nama: Maura, Alamat: Bintara 6, No.Telp:087883520021 ", dateTime = "16:42 PM "))
-        listChat.add(Chat(message = "Oke sis terima kasih, akan segera saya kirim ya, ditunggu nomor resi nya ", dateTime = "16:42 PM "))
+        btnSendMessage.setOnClickListener{
+            Toast.makeText(this@MainChat, "button send clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        input_message.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEND ) {
+                val idTypeChat = radiogroup_chat.checkedRadioButtonId
+                val typeChat = if(idTypeChat == R.id.radioButton_myself){
+                    AdapterChat.VIEW_TYPE_MY_SELF
+                } else {
+                    AdapterChat.VIEW_TYPE_USER
+                }
+                val message = input_message.text.toString().trim()
+                if (message.isEmpty()){
+                    Toast.makeText(this@MainChat, "Message is empty", Toast.LENGTH_SHORT).show()
+                } else {
+                    val dateTime = SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.US).format(Date())
+                    val chat = Chat(message = message, dateTime = dateTime)
+                    listViewType.add(typeChat)
+                    listChat.add(chat)
+                    adapterChat.notifyDataSetChanged()
+                    input_message.setText("")
+
+                    var im = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    im.hideSoftInputFromWindow(currentFocus?.windowToken, 0 )
 
 
-        val adapterChat = AdapterChat(listViewType = listViewType, listChat = listChat)
+
+
+                }
+
+            }
+            true
+        }
+        setupAdapterRecyclerView()
+
+
+}
+
+    private fun setupAdapterRecyclerView() {
+        listViewType = mutableListOf()
+        listChat = mutableListOf()
+        adapterChat = AdapterChat(listViewType = listViewType, listChat = listChat)
         chatRecyclerView.layoutManager = LinearLayoutManager(this)
         chatRecyclerView.adapter = adapterChat
     }
+
+
 }
