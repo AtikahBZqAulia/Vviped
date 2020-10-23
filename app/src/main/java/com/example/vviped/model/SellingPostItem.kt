1f62f62f5d16449816694e4c4d5c0c808f388869
AtikahBZqAulia <atikahap.yes@gmail.com>
2020-10-23 14:22:08 +0700

    Update SellingPostItem.kt

--- app/src/main/java/com/example/vviped/model/SellingPostItem.kt
+++ app/src/main/java/com/example/vviped/model/SellingPostItem.kt
@@ -1,8 +1,11 @@
 package com.example.vviped.model
 
+import android.os.Parcelable
 import com.example.vviped.R
+import kotlinx.android.parcel.Parcelize
 
-class SellingPostItem (
+@Parcelize
+data class SellingPostItem (
     val usernamepost: String,
     val user_profpict: Int,
     val image_post: Int,
@@ -10,4 +13,6 @@ class SellingPostItem (
     val product_price: String,
     val product_description: String,
     val seller_location: String
-)
+) : Parcelable{
+        val attributionUrl get() = "http://127.0.0.1:5000/api/v1/search?object=$product_name"
+}
