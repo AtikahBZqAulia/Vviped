7de0a2fbb6c8de9a45fb663b9b208ae573e54fed
mauraqp <mauraqoonitah@gmail.com>
2020-11-04 02:22:27 +0700

    hapus badge campaign

---
+++ app/src/main/java/com/example/vviped/create_campaign.kt
@@ -0,0 +1,11 @@
+package com.example.vviped
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create_campaign : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_campaign)
+    }
+}
