0eb18072fac59192bbd1a4ff98a1d9f30eb9b24b
mauraqp <mauraqoonitah@gmail.com>
2020-11-10 22:51:08 +0700

    remove folder

--- app/src/main/java/com/example/vviped/model/NotificationItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.vviped.model
-
-import android.media.Image
-
-class NotificationItem (
-    val titleNotif: String,
-    val descNotif: String,
-    val imageNotif: Int
-)
--- app/src/main/java/com/example/vviped/model/UploadResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.vviped.model
-
-data class UploadResponse (
-    val error: Boolean,
-    val message: String,
-    val image: String
-)
