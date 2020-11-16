c2a1d70e6686fb17cd6b27a68ae09be7c82f3ce6
AtikahBZqAulia <atikahap.yes@gmail.com>
2020-11-16 13:23:48 +0700

    Update SellingPostItem.kt

--- app/src/main/java/com/example/vviped/model/SellingPostItem.kt
+++ app/src/main/java/com/example/vviped/model/SellingPostItem.kt
@@ -2,18 +2,28 @@ package com.example.vviped.model
 
 import android.os.Parcelable
 import com.example.vviped.R
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class SellingPostItem (
+//    @SerializedName("username")
     val usernamepost: String,
+//    @SerializedName("user_profpict")
     val user_profpict: Int,
+//    @SerializedName("path")
     val image_post: Int,
+    @SerializedName("product_name")
     val product_name: String,
+    @SerializedName("product_price")
     val product_price: String,
+    @SerializedName("product_desc")
     val product_description: String,
+    @SerializedName("seller_loc")
     val seller_location: String,
+    @SerializedName("selling_status")
     val sold: String
+
 ) : Parcelable{
-        val attributionUrl get() = "http://127.0.0.1:5000/api/v1/search?object=$product_name"
+        val attributionUrl get() = "https://jakartaqurban.com/api/v1/search?object=$product_name"
 }
