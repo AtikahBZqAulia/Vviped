9d3147e52f83a3919a9ef14d6f3daa37010363fd
AtikahBZqAulia <atikahap.yes@gmail.com>
2020-11-23 21:02:27 +0700

    Update CampaignListAdapter.kt

--- app/src/main/java/com/example/vviped/model/CampaignListAdapter.kt
+++ app/src/main/java/com/example/vviped/model/CampaignListAdapter.kt
@@ -11,6 +11,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vviped.R
 import com.example.vviped.UploadSellingActivity
+import com.squareup.picasso.Picasso
 
 class CampaignListAdapter(
     private var context: Context,
@@ -34,8 +35,8 @@ class CampaignListAdapter(
 
         fun bindView(campaignItem: CampaignItem) {
             organizationnamepost.text = campaignItem.organization_name
-            organizationprofpict_post.setImageResource(campaignItem.organization_profpict)
-            imagecampaign.setImageResource(campaignItem.image_campaign)
+            Picasso.get().load(campaignItem.organization_profpict).into(organizationprofpict_post)
+            Picasso.get().load(campaignItem.image_campaign).into(imagecampaign)
             campaignname.text = campaignItem.campaign_name
             campaigndesc.text = campaignItem.campaign_desc
 
