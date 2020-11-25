0b0430e29e7c52ad9d8e4e16bc209dfc25808a48
octarinasalsa03 <octarinasalsabila@gmail.com>
2020-11-25 17:03:09 +0700

    show user data in Profile Fragment

--- app/src/main/java/com/example/vviped/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/vviped/ui/ProfileFragment.kt
@@ -1,19 +1,18 @@
 package com.example.vviped.ui
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vviped.*
+import com.example.vviped.model.login.Constant
 import com.example.vviped.model.login.Logout
 import com.example.vviped.model.login.PreferenceHelper
 import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 
 /**
@@ -30,13 +29,18 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
         return view
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val sharedPref = PreferenceHelper(requireActivity())
+
+        view.text_username.text = sharedPref.getString(Constant.PREF_USERNAME)
+        view.text_fullname.text = sharedPref.getString(Constant.PREF_FULLNAME)
+        view.user_email.text = sharedPref.getString(Constant.PREF_EMAIL)
 
         editprofile_btn.setOnClickListener{
             (context as MainActivity).changeFragment(EditProfileFragment())
