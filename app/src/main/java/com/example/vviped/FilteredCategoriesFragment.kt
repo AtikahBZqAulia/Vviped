86bf53acc61f4cac2bea6773baba57e498392a8e
mauraqp <mauraqoonitah@gmail.com>
2020-11-30 17:17:58 +0700

    add layout fragment when categories button clicked

--- app/src/main/java/com/example/vviped/FilteredCategoriesFragment.kt
+++ app/src/main/java/com/example/vviped/FilteredCategoriesFragment.kt
@@ -2,33 +2,42 @@ package com.example.vviped
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vviped.MainActivity
+import com.example.vviped.R
+import com.example.vviped.model.SellingPostItem
+import com.example.vviped.model.SellingPostProfileAdapter
+import com.example.vviped.model.SellingPostRepository
+import com.example.vviped.model.SellingPostsAdapter
 import com.example.vviped.ui.HomeFragment
 import com.example.vviped.ui.ProfileFragment
 import kotlinx.android.synthetic.main.activity_chat_for_buying.*
 import kotlinx.android.synthetic.main.fragment_edit_profile.*
 import kotlinx.android.synthetic.main.fragment_edit_profile.backspace_btn
 import kotlinx.android.synthetic.main.fragment_filtered_categories.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FilteredCategoriesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FilteredCategoriesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
+    private var recyclerView: RecyclerView? = null
+    private var sellingPostAdapter: SellingPostsAdapter? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,7 +56,11 @@ class FilteredCategoriesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filtered_categories, container, false)
+        val view =  inflater.inflate(R.layout.fragment_filtered_categories, container, false)
+        getData()
+        return view
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -57,10 +70,35 @@ class FilteredCategoriesFragment : Fragment() {
             onBackPressed()
         }
 
+        recyclerView = view.findViewById(R.id.recycleView_home)
+        recyclerView?.setHasFixedSize(true)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+
+
+
     }
 
     fun onBackPressed() {
         (context as MainActivity).changeFragment(HomeFragment())
+    }
+
+    fun getData(){
+
+        val feedsService = SellingPostRepository.create()
+        feedsService.getFeeds().enqueue(object : Callback<MutableList<SellingPostItem>> {
+            override fun onResponse(
+                call: Call<MutableList<SellingPostItem>>,
+                response: Response<MutableList<SellingPostItem>>
+            ) {
+                sellingPostAdapter = context?.let { SellingPostsAdapter(it, response.body() as MutableList<SellingPostItem>) }
+                recyclerView?.adapter = sellingPostAdapter
+                sellingPostAdapter?.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<MutableList<SellingPostItem>>, t: Throwable) {
+                Log.e("tag", t.toString())
+            }
+        })
     }
 
 }
