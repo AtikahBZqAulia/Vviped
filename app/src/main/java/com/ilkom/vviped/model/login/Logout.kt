866b6259913b623718aeb7e27846e315a1e5ff21
octarinasalsa03 <octarinasalsabila@gmail.com>
2020-12-16 17:03:21 +0700

    add logging when logout

--- app/src/main/java/com/ilkom/vviped/model/login/Logout.kt
+++ app/src/main/java/com/ilkom/vviped/model/login/Logout.kt
@@ -8,7 +8,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.ilkom.vviped.Landing
 import com.ilkom.vviped.MainActivity
 import com.ilkom.vviped.R
+import com.ilkom.vviped.model.RetrofitInterface
+import com.ilkom.vviped.model.UploadResponse
 import kotlinx.android.synthetic.main.activity_logout.*
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Logout : AppCompatActivity() {
     lateinit var sharedPref: PreferenceHelper
@@ -25,6 +32,21 @@ class Logout : AppCompatActivity() {
             startActivity(intent)
         }
         btn_yes.setOnClickListener{
+            RetrofitInterface().userActivities(
+                sharedPref.getInt(Constant.PREF_ID)!!,
+                sharedPref.getString(Constant.PREF_USERNAME)!!,
+                RequestBody.create(MediaType.parse("multipart/form-data"), "Logout from apps"),
+            ).enqueue(object : Callback<UploadResponse> {
+                override fun onFailure(call: Call<UploadResponse>, t: Throwable) {
+                }
+
+                override fun onResponse(
+                    call: Call<UploadResponse>,
+                    response: Response<UploadResponse>
+                ) {
+
+                }
+            })
             sharedPref.clear()
 
             startActivity(Intent(this, Landing::class.java).also {
--- app/src/main/java/com/ilkom/vviped/settings/SettingsActivity.kt
+++ app/src/main/java/com/ilkom/vviped/settings/SettingsActivity.kt
@@ -1,14 +1,24 @@
 package com.ilkom.vviped.settings
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.ilkom.vviped.EditProfileUser
 import com.ilkom.vviped.R
+import com.ilkom.vviped.model.RetrofitInterface
+import com.ilkom.vviped.model.UploadResponse
+import com.ilkom.vviped.model.login.Constant
 import com.ilkom.vviped.model.login.Logout
+import com.ilkom.vviped.model.login.PreferenceHelper
 import kotlinx.android.synthetic.main.activity_settings.*
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -26,6 +36,23 @@ class SettingsActivity : AppCompatActivity() {
         btn_settings_logout.setOnClickListener{
             val intent = Intent(this, Logout::class.java)
             startActivity(intent)
+
+            val sharedPref = PreferenceHelper(this)
+            RetrofitInterface().userActivities(
+                sharedPref.getInt(Constant.PREF_ID)!!,
+                sharedPref.getString(Constant.PREF_USERNAME)!!,
+                RequestBody.create(MediaType.parse("multipart/form-data"), "Click Logout button"),
+            ).enqueue(object : Callback<UploadResponse> {
+                override fun onFailure(call: Call<UploadResponse>, t: Throwable) {
+                }
+
+                override fun onResponse(
+                    call: Call<UploadResponse>,
+                    response: Response<UploadResponse>
+                ) {
+
+                }
+            })
         }
         btn_settings_edit_profile.setOnClickListener {
             val intent = Intent(this, EditProfileUser::class.java)
