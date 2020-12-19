1d62d7b179854292881086c4f283b16ad5a46a6f
mauraqp <mauraqoonitah@gmail.com>
2020-12-20 04:55:08 +0700

    add context menu pop up

--- app/src/main/java/com/ilkom/vviped/model/SellingPostProfileAdapter.kt
+++ app/src/main/java/com/ilkom/vviped/model/SellingPostProfileAdapter.kt
@@ -1,15 +1,15 @@
 package com.ilkom.vviped.model
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.RecyclerView
-import com.ilkom.vviped.BottomSheetProfilePost
 import com.ilkom.vviped.R
 import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 class SellingPostProfileAdapter(
     private val context: Context,
@@ -28,7 +28,7 @@ class SellingPostProfileAdapter(
         val productdesc = itemView.findViewById<TextView>(R.id.produkdeskripsi)
         val sellerlocation = itemView.findViewById<TextView>(R.id.lokasipenjual)
         val soldTextView = itemView.findViewById<TextView>(R.id.sale_tv)
-        val buttonmenu = itemView.findViewById<ImageButton>(R.id.btn_menu_post)
+        val buttonContextMenu = itemView.findViewById<ImageButton>(R.id.btn_context_menu)
 
 
         fun bindView(sellingPost: SellingPostItem) {
@@ -43,14 +43,39 @@ class SellingPostProfileAdapter(
             sellerlocation.text = sellingPost.seller_location
             soldTextView.text = sellingPost.sold
 
-            val bottomSheetProfilePost = BottomSheetProfilePost()
 
-            buttonmenu.setOnClickListener {
-                val context = buttonmenu.context
-                Toast.makeText(context, "menu button", Toast.LENGTH_SHORT).show()
+            buttonContextMenu.setOnClickListener {
+                val context = buttonContextMenu.context
+                val pop= PopupMenu(context, it)
+                pop.inflate(R.menu.context_menu_post)
+
+                pop.setOnMenuItemClickListener { item ->
+                    when(item.itemId){
+                        R.id.menu_edit_post->{
+                            Toast.makeText(context, "edit", Toast.LENGTH_SHORT).show()
+
+                        }
+                        R.id.menu_delete_post->{
+                            Toast.makeText(context, "delete", Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                    true
+                }
+                try {
+                    val fieldMPopup = PopupMenu::class.java.getDeclaredField("mPopup")
+                    fieldMPopup.isAccessible = true
+                    val mPopup = fieldMPopup.get(pop)
+                    mPopup.javaClass
+                        .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+                        .invoke(mPopup, true)
+                } catch (e: Exception){
+                    Log.e("Main", "Error showing menu icons", e )
+
+                } finally {
+                    pop.show()
+                }
             }
-
-
 
         }
     }
