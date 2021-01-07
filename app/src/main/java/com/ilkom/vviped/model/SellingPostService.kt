cc1ca39b89ea179b82080cb94be99d2b07938f20
AtikahBZqAulia <atikahap.yes@gmail.com>
2021-01-07 16:26:32 +0700

    Update SellingPostService.kt

--- app/src/main/java/com/ilkom/vviped/model/SellingPostService.kt
+++ app/src/main/java/com/ilkom/vviped/model/SellingPostService.kt
@@ -1,16 +1,15 @@
 package com.ilkom.vviped.model
-
-import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.*
-import retrofit2.http.Path
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface SellingPostService {
     @GET("api.php?apicall=sellingproducts")
     fun getFeeds(): Call<MutableList<SellingPostItem>>
 
-    @GET("detect/object?={object_name}")
-    fun detectedObject(@Path("object_name") nameObject: String):Call<MutableList<DetectedItem>>
-}
+    @GET("detect")
+    fun detectedObject(@Query("link") objectName: String): Call<List<DetectedItem>>
 
+
+}
 
